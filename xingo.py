6736980171d73e83a89e1f052a672683cb70783d
TiaLiliUwU <henderson12souza@gmail.com>
2023-01-20 20:03:38 -0300

    otimização do código

--- xingo.py
+++ xingo.py
@@ -8,24 +8,19 @@
 
 # bibliotecas para gerenciar menus
 import os
-import platform
-so = platform.system()
 #
 # Comando Clear
 def clear():
-    if so == 'Windows':
-        os.system('clr')
-    else:
-        os.system('clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 #
 # xingamentos pseudoaleatórios
 import random
 x = ['Vai tomar no cu,', 'Vai se foder,', 'Você é um lixo,', 'Você é um desperdício de oxigênio,', 'Você é um bosta,', 'Você é a vergonha da família,', 'Nem sua mãe gosta de você,', 'Some daqui,']
-xnum = [] # xnum = número pseudoaleatório que gera um xingamento pseudoaleatório
+x_util = []
 #
 # lista de pessoas
 ppls = [] # ppls = lista de pessoas
-pplsx = [] # pplsx = lisa de xingamentos usados para as pessoas xingadas
+pplsx = [] # pplsx = lisa de xingamento + pessoa xingadas
 #
 # Swap
 txt_nomes = 'nomes.txt'
@@ -45,7 +40,7 @@ if os.path.exists('xingos.txt'):
     with open(txt_xingos) as xingostxt:
         xi = xingostxt.read().splitlines()
         for ln in xi:
-            xnum.append(int(ln))
+            x_util.append(ln)
     xingostxt.close()
 else:
     pass
@@ -63,7 +58,15 @@ else:
 clear()
 print('Bem vindo ao gerador de xingos pseudoaleatórios! By: Mimus \n')
 #
-# Funcionalidades
+# Funcionalidades:
+#
+# Mensagem de erro
+def erro():
+    print('Selecione uma opção válida... \n')
+    input('Aperte enter para continuar...')
+    clear()
+#
+#
 while True:
     print('O que deseja fazer? \n\n1 - Xingar alguém \n2 - Pessoas xingadas \n3 - Xingamentos usados \n4 - Como cada pessoa foi xingada \n5 - Sair \n')
     opc = input('Selecione a opção desejada: ') # opc = opção do menu principal
@@ -71,13 +74,12 @@ while True:
 # Xingar alguém
     if opc == '1':
         ppl = input('Quem deseja xingar? ') # ppl = nome da pessoa a ser xingada
-        ppls.append(ppl) # ppls = lista de pessas xingadas
         print()
-        # Gerador pseudoaleatório de xingos
-        alt_num = random.randint(0,7) # alt_num = número pseudoaleatório gerado pelo "import random"
-        xnum.append(alt_num) # xnum = número pseudoaleatório que gera um xingamento pseudoaleatório
-        pplx = (x[alt_num] + ' ' + ppl + '!') # pplx = xingamento + pessoa xingadas
-        pplsx.append(pplx) # pplsx = lisa de xingamentos usados para as pessoas xingadas
+        ppls.append(ppl) # ppls = lista de pessas xingadas
+        xing = random.choice(x) # gerador pseudoaleatório de xingos
+        x_util.append(xing) # lista de xingamentos utilizados
+        pplx = (xing + ' ' + ppl + '!') # pplx = xingamento + pessoa xingadas
+        pplsx.append(pplx) # pplsx = lisa de xingamento + pessoa xingadas
         print(pplx, '\n')
         input('Aperte enter para continuar...')
         clear()
@@ -87,40 +89,23 @@ while True:
         print('O que deseja fazer? \n\n1 - Ver últimas 5 pessoas xingadas \n2 - Ver todas as pessoas xingadas \n3 - Retornar \n')
         sel = input('Selecione a opção desejada: ')
         clear()
+        verific = len(ppls)
+        def verific_ppls():
+            print('Ninguém foi xingado... \n')
+            input('Aperte enter para continuar...')
+            clear()
         if sel == '1':
-            verific = len(ppls)
             if verific == 0:
-                print('Ninguém foi xingado... \n')
-                input('Aperte enter para continuar...')
-                clear()
+                verific_ppls()
             else:
                 print('Foram xingados: \n')
-                if verific == 1:
-                    print(ppls[verific - 1])
-                    input('\nAperte enter para continuar...')
-                    clear()
-                elif verific == 2:
-                    print(ppls[verific - 2] + '\n' + ppls[verific - 1])
-                    input('\nAperte enter para continuar...')
-                    clear()
-                elif verific == 3:
-                    print(ppls[verific - 3] + '\n' + ppls[verific - 2] + '\n' + ppls[verific - 1])
-                    input('\nAperte enter para continuar...')
-                    clear()
-                elif verific == 4:
-                    print(ppls[verific - 4] + '\n' + ppls[verific - 3] + '\n' + ppls[verific - 2] + '\n' + ppls[verific - 1])
-                    input('\nAperte enter para continuar...')
-                    clear()
-                elif verific >= 5:
-                    print(ppls[verific - 5] + '\n' + ppls[verific - 4] + '\n' + ppls[verific - 3] + '\n' + ppls[verific - 2] + '\n' + ppls[verific - 1])
-                    input('\nAperte enter para continuar...')
-                    clear()
-        elif sel =='2':
-            verific = len(ppls)
-            if verific == 0:
-                print('Ninguém foi xingado... \n')
-                input('Aperte enter para continuar...')
+                for i in ppls[-5:]:
+                    print(i)
+                input('\nAperte enter para continuar...')
                 clear()
+        elif sel =='2':
+            if verific == 0:
+                verific_ppls()
             else:
                 print('Foram xingados: \n')
                 for pp in ppls: # pp = pessoa na lista de pessoas xingadas & ppls = lista de pessoas
@@ -130,100 +115,62 @@ while True:
         elif sel == '3':
             clear()
         else:
-            print('Selecione uma opção válida... \n')
-            input('Aperte enter para continuar...')
-            clear()
+            erro()
 #
 # Ver xingamentos usados
     elif opc == '3':
         print('O que deseja fazer? \n\n1 - Ver últimos 5 xingamentos usados \n2 - Ver todos os xingamentos usados \n3 - Retornar \n')
         sel = input('Selecione a opção desejada: ')
         clear()
+        verific = len(x_util)
+        def verific_x_util():
+            print('Ninguém foi xingado... \n')
+            input('Aperte enter para continuar...')
+            clear()
         if sel == '1':
-            verific = len(xnum)
             if verific == 0:
-                print('Ninguém foi xingado... \n')
-                input('Aperte enter para continuar...')
-                clear()
+                verific_x_util()
             else:
                 print('Foram utilizados: \n')
-                if verific == 1:
-                    print(x[xnum[verific - 1]])
-                    input('\nAperte enter para continuar...')
-                    clear()
-                elif verific == 2:
-                    print(x[xnum[verific - 2]] + '\n' + x[xnum[verific - 1]])
-                    input('\nAperte enter para continuar...')
-                    clear()
-                elif verific == 3:
-                    print(x[xnum[verific - 3]] + '\n' + x[xnum[verific - 2]] + '\n' + x[xnum[verific - 1]])
-                    input('\nAperte enter para continuar...')
-                    clear()
-                elif verific == 4:
-                    print(x[xnum[verific - 4]] + '\n' + x[xnum[verific - 3]] + '\n' + x[xnum[verific - 2]] + '\n' + x[xnum[verific - 1]])
-                    input('\nAperte enter para continuar...')
-                    clear()
-                elif verific >= 5:
-                    print(x[xnum[verific - 4]] + '\n' + x[xnum[verific - 4]] + '\n' + x[xnum[verific - 3]] + '\n' + x[xnum[verific - 2]] + '\n' + x[xnum[verific - 1]])
-                    input('\nAperte enter para continuar...')
-                    clear()
+                for i in x_util[-5:]:
+                    print(i)
+                input('\nAperte enter para continuar...')
+                clear()
         elif sel =='2':
-            verific = len(xnum)
             if verific == 0:
-                print('Ninguém foi xingado... \n')
-                input('Aperte enter para continuar...')
-                clear()
+                verific_x_util()
             else:
                 print('Foram utilizados: \n')
-                for num in xnum: # num = número na lista de xnum & xnum = número pseudoaleatório que gera um xingamento pseudoaleatório
-                    print(x[num]) # x = lista de Xingamentos
+                for num in x_util: # num = xingo na lista x_util
+                    print(num) # x = lista de Xingamentos
                 input('\nAperte enter para continuar...')
                 clear()
         elif sel == '3':
             clear()
         else:
-            print('Selecione uma opção válida... \n')
-            input('Aperte enter para continuar...')
-            clear()
+            erro()
 # Ver como cada pessoa foi xingada
     elif opc == '4':
         print('O que deseja fazer? \n\n1 - Ver últimas 5 pessoas e seus xingamentos \n2 - Ver todas as pessoas e seus xingamentos \n3 - Retornar \n')
         sel = input('Selecione a opção desejada: ')
         clear()
+        verific = len(pplsx)
+        def verific_pplsx():
+            print('Ninguém foi xingado... \n')
+            input('Aperte enter para continuar...')
+            clear()
         if sel == '1':
-            verific = len(pplsx)
             if verific == 0:
-                print('Ninguém foi xingado... \n')
-                input('Aperte enter para continuar...')
-                clear()
+                verific_pplsx()
             else:
                 print('Cada pessoa foi xingada da seguinte forma: \n')
-                if verific == 1:
-                    print(pplsx[verific - 1])
-                    input('\nAperte enter para continuar...')
-                    clear()
-                elif verific == 2:
-                    print(pplsx[verific - 2] + '\n' + pplsx[verific - 1])
-                    input('\nAperte enter para continuar...')
-                    clear()
-                elif verific == 3:
-                    print(pplsx[verific - 3] + '\n' + pplsx[verific - 2] + '\n' + pplsx[verific - 1])
-                    input('\nAperte enter para continuar...')
-                    clear()
-                elif verific == 4:
-                    print(pplsx[verific - 4] + '\n' + pplsx[verific - 3] + '\n' + pplsx[verific - 2] + '\n' + pplsx[verific - 1])
-                    input('\nAperte enter para continuar...')
-                    clear()
-                elif verific >= 5:
-                    print(pplsx[verific - 5] + '\n' + pplsx[verific - 4] + '\n' + pplsx[verific - 3] + '\n' + pplsx[verific - 2] + '\n' + pplsx[verific - 1])
-                    input('\nAperte enter para continuar...')
-                    clear()
-        elif sel =='2':
-            verific = len(pplsx)
-            if verific == 0:
-                print('Ninguém foi xingado... \n')
-                input('Aperte enter para continuar...')
+                for i in pplsx[-5:]:
+                    print(i)
+                input('\nAperte enter para continuar...')
                 clear()
+        elif sel =='2':
+            if verific == 0:
+                verific_pplsx()
             else:
                 print('Cada pessoa foi xingada da seguinte forma: \n')
                 for px in pplsx: # px = pessoa + xingamento na lista de pplsx & pplsx = lisa de xingamentos usados para as pessoas xingadas
@@ -233,9 +180,7 @@ while True:
         elif sel == '3':
             clear()
         else:
-            print('Selecione uma opção válida... \n')
-            input('Aperte enter para continuar...')
-            clear()
+            erro()
 #
 # Sair
     elif opc == '5':
@@ -244,8 +189,8 @@ while True:
                 nomestxt.write(nm + '\n')
         nomestxt.close()
         with open(txt_xingos, 'w+') as xingostxt:
-            for lt in xnum:
-                xingostxt.write(str(lt) + '\n')
+            for lt in x_util:
+                xingostxt.write(lt + '\n')
         xingostxt.close()
         with open(txt_nomes_xingos, 'w+') as nomes_xingostxt:
             for xn in pplsx:
@@ -254,6 +199,5 @@ while True:
         print('Que a força do ódio esteja com você ;)')
         exit()
     else:
-        input('Por favor, selecione uma opção válida. \nAperte enter para continuar...')
-        clear()
+        erro()
 #
