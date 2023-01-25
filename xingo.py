e177151f116bf3b5291d852fbdd3f1950b58a953
TiaLiliUwU <henderson12souza@gmail.com>
2023-01-24 22:41:57 -0300

    alfa da versão 1.2.0

--- xingo.py
+++ xingo.py
@@ -6,12 +6,10 @@
 
 ###################################################
 
-# bibliotecas para gerenciar menus
+# Gerenciar menus
 import os
-#
-# Comando Clear
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
+menu = ['1', '2', '3', '4', '5', '6']
+sub_menu = ['1', '2', '3']
 #
 # xingamentos pseudoaleatórios
 import random
@@ -54,23 +52,53 @@ if os.path.exists('nomes_xingos.txt'):
 else:
     pass
 #
+# Funcionalidades:
+#
+# Comando Clear
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+#
+# Aperte enter para continuar
+def enter():
+    ctrl = True
+    input('\nAperte enter para continuar...')
+    clear()
+#
+# Mensagem de erro
+def erro():
+    print('Selecione uma opção válida...')
+    enter()
+#
+# Menu clear
+def opc_clear():
+    if opc in menu:
+        clear()
+    else:
+        pass
+#
+# Verificação
+def veri():
+    print('Não há dados registrados...')
+    enter()
+#
+# Submenu clear
+def subclear():
+    if sel in sub_menu:
+        clear()
+    else:
+        pass
+
+#
 # cabeçalho
 clear()
 print('Bem vindo ao gerador de xingos pseudoaleatórios! By: Mimus \n')
 #
-# Funcionalidades:
-#
-# Mensagem de erro
-def erro():
-    print('Selecione uma opção válida... \n')
-    input('Aperte enter para continuar...')
-    clear()
-#
-#
 while True:
-    print('O que deseja fazer? \n\n1 - Xingar alguém \n2 - Pessoas xingadas \n3 - Xingamentos usados \n4 - Como cada pessoa foi xingada \n5 - Sair \n')
+    ctrl = True
+    print('O que deseja fazer? \n\n1 - Xingar alguém \n2 - Pessoas xingadas \n3 - Xingamentos usados \n4 - Como cada pessoa foi xingada \n5 - Reset \n6 - Sair \n')
     opc = input('Selecione a opção desejada: ') # opc = opção do menu principal
-    clear()
+    opc_clear()
+#
 # Xingar alguém
     if opc == '1':
         ppl = input('Quem deseja xingar? ') # ppl = nome da pessoa a ser xingada
@@ -80,110 +108,130 @@ while True:
         x_util.append(xing) # lista de xingamentos utilizados
         pplx = (xing + ' ' + ppl + '!') # pplx = xingamento + pessoa xingadas
         pplsx.append(pplx) # pplsx = lisa de xingamento + pessoa xingadas
-        print(pplx, '\n')
-        input('Aperte enter para continuar...')
-        clear()
+        print(pplx)
+        enter()
 #
 # Ver todas as pessoas xingadas
     elif opc == '2':
-        print('O que deseja fazer? \n\n1 - Ver últimas 5 pessoas xingadas \n2 - Ver todas as pessoas xingadas \n3 - Retornar \n')
-        sel = input('Selecione a opção desejada: ')
-        clear()
-        verific = len(ppls)
-        def verific_ppls():
-            print('Ninguém foi xingado... \n')
-            input('Aperte enter para continuar...')
-            clear()
-        if sel == '1':
-            if verific == 0:
-                verific_ppls()
+        while ctrl == True:
+            ctrl = False
+            print('O que deseja fazer? \n\n1 - Ver últimas 5 pessoas xingadas \n2 - Ver todas as pessoas xingadas \n3 - Retornar \n')
+            sel = input('Selecione a opção desejada: ')
+            verific = len(ppls)
+            subclear()
+            if sel == '1':
+                if verific == 0:
+                    veri()
+                else:
+                    print('Foram xingados: \n')
+                    for i in ppls[-5:]:
+                        print(i)
+                    enter()
+            elif sel =='2':
+                if verific == 0:
+                    veri()
+                else:
+                    print('Foram xingados: \n')
+                    for pp in ppls: # pp = pessoa na lista de pessoas xingadas & ppls = lista de pessoas
+                        print(pp) # pp = pessoa na lista de pessoas xingadas
+                    enter()
+            elif sel == '3':
+                pass
             else:
-                print('Foram xingados: \n')
-                for i in ppls[-5:]:
-                    print(i)
-                input('\nAperte enter para continuar...')
-                clear()
-        elif sel =='2':
-            if verific == 0:
-                verific_ppls()
-            else:
-                print('Foram xingados: \n')
-                for pp in ppls: # pp = pessoa na lista de pessoas xingadas & ppls = lista de pessoas
-                    print(pp) # pp = pessoa na lista de pessoas xingadas
-                input('\nAperte enter para continuar...')
-                clear()
-        elif sel == '3':
-            clear()
-        else:
-            erro()
+                print()
+                ctrl = True
+                erro()
 #
 # Ver xingamentos usados
     elif opc == '3':
-        print('O que deseja fazer? \n\n1 - Ver últimos 5 xingamentos usados \n2 - Ver todos os xingamentos usados \n3 - Retornar \n')
-        sel = input('Selecione a opção desejada: ')
-        clear()
-        verific = len(x_util)
-        def verific_x_util():
-            print('Ninguém foi xingado... \n')
-            input('Aperte enter para continuar...')
-            clear()
-        if sel == '1':
-            if verific == 0:
-                verific_x_util()
+        while ctrl == True:
+            ctrl = False
+            print('O que deseja fazer? \n\n1 - Ver últimos 5 xingamentos usados \n2 - Ver todos os xingamentos usados \n3 - Retornar \n')
+            sel = input('Selecione a opção desejada: ')
+            verific = len(x_util)
+            subclear()
+            if sel == '1':
+                if verific == 0:
+                    veri()
+                else:
+                    print('Foram utilizados: \n')
+                    for i in x_util[-5:]:
+                        print(i)
+                    enter()
+            elif sel =='2':
+                if verific == 0:
+                    veri()
+                else:
+                    print('Foram utilizados: \n')
+                    for num in x_util: # num = xingo na lista x_util
+                        print(num) # x = lista de Xingamentos
+                    enter()
+            elif sel == '3':
+                pass
             else:
-                print('Foram utilizados: \n')
-                for i in x_util[-5:]:
-                    print(i)
-                input('\nAperte enter para continuar...')
-                clear()
-        elif sel =='2':
-            if verific == 0:
-                verific_x_util()
-            else:
-                print('Foram utilizados: \n')
-                for num in x_util: # num = xingo na lista x_util
-                    print(num) # x = lista de Xingamentos
-                input('\nAperte enter para continuar...')
-                clear()
-        elif sel == '3':
-            clear()
-        else:
-            erro()
+                print()
+                ctrl = True
+                erro()
 # Ver como cada pessoa foi xingada
     elif opc == '4':
-        print('O que deseja fazer? \n\n1 - Ver últimas 5 pessoas e seus xingamentos \n2 - Ver todas as pessoas e seus xingamentos \n3 - Retornar \n')
-        sel = input('Selecione a opção desejada: ')
-        clear()
-        verific = len(pplsx)
-        def verific_pplsx():
-            print('Ninguém foi xingado... \n')
-            input('Aperte enter para continuar...')
-            clear()
-        if sel == '1':
-            if verific == 0:
-                verific_pplsx()
+        while ctrl == True:
+            ctrl = False
+            print('O que deseja fazer? \n\n1 - Ver últimas 5 pessoas e seus xingamentos \n2 - Ver todas as pessoas e seus xingamentos \n3 - Retornar \n')
+            sel = input('Selecione a opção desejada: ')
+            verific = len(pplsx)
+            subclear()
+            if sel == '1':
+                if verific == 0:
+                    veri()
+                else:
+                    print('Cada pessoa foi xingada da seguinte forma: \n')
+                    for i in pplsx[-5:]:
+                        print(i)
+                    enter()
+            elif sel =='2':
+                if verific == 0:
+                    veri()
+                else:
+                    print('Cada pessoa foi xingada da seguinte forma: \n')
+                    for px in pplsx: # px = pessoa + xingamento na lista de pplsx & pplsx = lisa de xingamentos usados para as pessoas xingadas
+                        print(px) # px = pessoa + xingamento na lista de pplsx
+                    enter()
+            elif sel == '3':
+                pass
             else:
-                print('Cada pessoa foi xingada da seguinte forma: \n')
-                for i in pplsx[-5:]:
-                    print(i)
-                input('\nAperte enter para continuar...')
-                clear()
-        elif sel =='2':
-            if verific == 0:
-                verific_pplsx()
+                print()
+                ctrl = True
+                erro()
+#
+# Reset
+    elif opc == '5':
+        while ctrl == True:
+            ctrl = False
+            print('Tem certeza que deseja excluir os dados armazenados? \n\n1 - Sim \n2 - Não \n')
+            ex_d = input('Selecione a opção desejada: ')
+            if ex_d == '1':
+                del ppls[0:], x_util[0:], pplsx[0:]
+                with open(txt_nomes, 'w+') as nomestxt:
+                    pass
+                nomestxt.close()
+                with open(txt_xingos, 'w+') as xingostxt:
+                    pass
+                xingostxt.close()
+                with open(txt_nomes_xingos, 'w+') as nomes_xingostxt:
+                    pass
+                nomes_xingostxt.close()
+                print('\nLista resetada com sucesso!')
+                enter()
+            elif ex_d == '2':
+                print('\nReset abortado!')
+                enter()
             else:
-                print('Cada pessoa foi xingada da seguinte forma: \n')
-                for px in pplsx: # px = pessoa + xingamento na lista de pplsx & pplsx = lisa de xingamentos usados para as pessoas xingadas
-                    print(px) # px = pessoa + xingamento na lista de pplsx
-                input('\nAperte enter para continuar...')
-                clear()
-        elif sel == '3':
-            clear()
-        else:
-            erro()
+                print()
+                ctrl = True
+                erro()
 #
 # Sair
-    elif opc == '5':
+    elif opc == '6':
         with open(txt_nomes, 'w+') as nomestxt:
             for nm in ppls:
                 nomestxt.write(nm + '\n')
@@ -199,5 +247,6 @@ while True:
         print('Que a força do ódio esteja com você ;)')
         exit()
     else:
+        print()
         erro()
 #
